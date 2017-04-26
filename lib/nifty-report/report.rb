6ee5f759d01b87a070b7b2d62fd235ed1c1f509f
Scott Albertson <ascottalbertson@gmail.com>
2017-04-25 20:44:33 -0700

    Add long line

--- lib/nifty-report/report.rb
+++ lib/nifty-report/report.rb
@@ -1,6 +1,6 @@
 require 'nifty-report/model'
 
-module Nifty              
+module Nifty
   class Report
     def initialize(sql, name, connection_string = nil)
       @sql = sql
@@ -35,6 +35,8 @@ module Nifty
       remote_file = AWS::S3::S3Object.find(filename, ENV['AWS_S3_BUCKET'])
 
       ReportMailer.report_email(email_address, remote_file.url(expires_in: 30 * 60)).deliver
+
+      wat = "aaaaaaaaaahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
     end
   end
 end
